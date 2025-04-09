b7c646241e8762f922ac15dee97fc54e73b354e5
George Tuges <george.tuges@live.com>
2025-04-10 07:55:23 +1000

    server created
    server

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./assets/utils/HomePage";
 import LoginPage from "./assets/utils/LoginPage";
 import UnknownPage from "./assets/utils/UnknownPage";
+import Users from "./assets/pages/Users";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   { path: "/login", element: <LoginPage /> },
+  { path: "/users", element: <Users /> },
   { path: "*", element: <UnknownPage /> },
 ]);
 
