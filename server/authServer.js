124027b7b9817c3cd2f51915c7d0a696a11060fd
George Tuges <george.tuges@live.com>
2025-04-12 15:16:08 +1000

    Completed jwt
    functional jwt

---
+++ server/authServer.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const PORT = 5002;
+require("dotenv").config();
+
+app.use(express.json());
+
+// import - Normal you would store your refresh token in form of database.
+// for this we will store it locally, this is not an ideal, when the server is rest, you loose all the stored tokens
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  // only refresh tokens are sent into this tooken
+  const refreshToken = req.body.token;
+
+  // console.log(` refreshtoken -> ${refreshToken}`);
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    // alwys check for errors
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello world</h1>");
+//   console.log("get send !");
+// });
+
+app.get("/posts", (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+app.post("/login", (req, res) => {
+  // authentication
+  // left off from here https://youtu.be/mbsmsi7l3r4?t=827
+  const username = req.body.username;
+  const user = { name: username };
+
+  // const accessToken = jwt.sign(user, process.env.ACCSS_TOKEN_SECRET);
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  // console.log(`login ${accessToken}`)
+
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "60s" });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT : ${PORT}`);
+});
--- server/server.js
+++ server/server.js
@@ -1,74 +1,52 @@
-
-const express = require('express')
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
-const PORT = 5001
-require('dotenv').config();
+const PORT = 5001;
+require("dotenv").config();
 
 app.use(express.json());
 
-const posts = [{
-    username : 'gtuges',
-    title : 'title 1'
-},
-{
-    username : 'stuges',
-    title : 'title 2'
-},
-{
-    username : 'jtuges',
-    title : 'title 3'
-}]
+const posts = [
+  {
+    username: "gtuges",
+    title: "title 1",
+  },
+  {
+    username: "stuges",
+    title: "title 2",
+  },
+  {
+    username: "jtuges",
+    title: "title 3",
+  },
+];
 
-app.get("/",(req,res) => {
-    res.send("<h1>Hello world</h1>");
-    console.log("get send !")
-})
-
-app.get('/posts',authenticateToken,(req,res) =>{
-
-    res.json(posts.filter(post => post.username === req.user.name)) 
-})
-
-
-app.post('/login',(req,res)=> {
-    // authentication
-    // left off from here https://youtu.be/mbsmsi7l3r4?t=827
-    const username = req.body.username;
-    const user = { name : username }
-
-    const accessToken = jwt.sign(user, process.env.ACCSS_TOKEN_SECRET);
-
-    console.log(`login ${accessToken}`)
-
-    res.json ( { accessToken : accessToken })
-
-
-})
-
-function authenticateToken(req, res, next) {
-
-    const authHeader = req.headers['authorization']
-    console.log(` authHeader : ${authHeader}`) 
-    
-    const token = authHeader && authHeader.split(' ')[1]
-    
-
-    // console.log(` Authenticate : ${token}`) 
-
-    if (token == null ) return res.sendStatus(401)
-
-        jwt.verify(token, process.env.ACCSS_TOKEN_SECRET,(err,user) => {
-            if(err) return res.sendStatus(403)
-            req.user = user    
-            next();
-        })
-
-}
-
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT : ${PORT}`)
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+  console.log("get send !");
 });
 
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  // console.log(` authHeader : ${authHeader}`)
 
+  const token = authHeader && authHeader.split(" ")[1];
+
+  // console.log(` Authenticate : ${token}`)
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT : ${PORT}`);
+});
